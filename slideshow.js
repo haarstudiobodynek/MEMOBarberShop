b4f36625448a078363c1754c38d8576cb873621c
haarstudiobodynek <110938039+haarstudiobodynek@users.noreply.github.com>
2022-11-13 12:15:14 +0100

    MEMOBarberShop 1st upload

---
+++ slideshow.js
@@ -0,0 +1,51 @@
+
+const indikatoren = document.getElementsByClassName("indikator");
+indikatoren[0].classList.add("aktiv");
+
+const slides = document.getElementsByClassName("slide");
+slides[0].classList.add("aktiv");
+
+var aktuellerIndex = 0;
+var letzteAktualisierung = new Date();
+
+function umschalten(anzahl) {
+    var neuerIndex = aktuellerIndex + anzahl;
+
+    if(neuerIndex < 0) {
+        neuerIndex = slides.length -1;
+    }
+
+    if(neuerIndex > slides.length -1) {
+        neuerIndex = 0;
+    }
+
+    springeZuEintrag(neuerIndex);
+}
+
+function springeZuEintrag(neuerIndex) {
+    indikatoren[aktuellerIndex].classList.remove("aktiv");
+    slides[aktuellerIndex].classList.remove("aktiv");
+
+    indikatoren[neuerIndex].classList.add("aktiv");
+    slides[neuerIndex].classList.add("aktiv");
+
+    aktuellerIndex = neuerIndex;
+    letzteAktualisierung = new Date();
+}
+
+function automatischweiterschalten() {
+    const vergangeneZeit = new Date() -letzteAktualisierung;
+
+    if(vergangeneZeit >= 8000) {
+        umschalten(1);
+    }
+}
+
+setInterval(automatischweiterschalten, 8000);
+
+
+
+
+
+
+
